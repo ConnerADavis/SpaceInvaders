4bf6e460f87983413cb5fd56b31b797f81c0236f
Conner Davis <conner.davis.17@cnu.edu>
2017-11-22 20:27:59 -0500

    Added in assets, wrote some of main class.

--- CPSC 280 Assignment 4/CPSC 280 Assignment 4 - Conner Davis/src/SI.java
+++ CPSC 280 Assignment 4/CPSC 280 Assignment 4 - Conner Davis/src/SI.java
@@ -1,6 +1,47 @@
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 public class SI extends JFrame {
+	
+	SIpanel panel = new SIpanel();
+	
+	public SI()
+	{
+		
+		setSize(500, 450);
+		setTitle("Space Invaders");
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+		addWindowListener(new WindowAdapter() 
+        {
+        	@Override
+        	public void windowClosing(WindowEvent arg0)
+        	{
+        		displayExitDialog();
+        	}
+        });
+	}
+	
+	private void displayAboutDialog()
+    {
+        JOptionPane.showMessageDialog(this, new JLabel("<html><hr><b>Space Invaders</b><br>by Conner A. Davis</hr></html>"), 
+                "Message",JOptionPane.INFORMATION_MESSAGE);
+    }
+	
+	private void displayExitDialog()
+    {
+		panel.dialogPause();
+    	int result = JOptionPane.showConfirmDialog(SI.this, "Dare to quit?");
+        if(result == JOptionPane.YES_OPTION)
+            dispose();
+        else
+        	panel.dialogUnpause();
+    }
 
     public static void main(String[] args) {
         JFrame f = new SI();
--- CPSC 280 Assignment 4/CPSC 280 Assignment 4 - Conner Davis/src/SIpanel.java
+++ CPSC 280 Assignment 4/CPSC 280 Assignment 4 - Conner Davis/src/SIpanel.java
@@ -1,5 +1,20 @@
 import javax.swing.JPanel;
 
 public class SIpanel extends JPanel {
+	
+	public SIpanel()
+	{
+		
+	}
+
+	public void dialogPause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void dialogUnpause() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
