b96d3bdcbb112e0cfd582a86a104d5672eb40de9
Conner Davis <conner.davis.17@cnu.edu>
2017-11-26 19:50:28 -0500

    Committing so I can work from my desktop

--- CPSC 280 Assignment 4/CPSC 280 Assignment 4 - Conner Davis/src/SI.java
+++ CPSC 280 Assignment 4/CPSC 280 Assignment 4 - Conner Davis/src/SI.java
@@ -1,8 +1,13 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 
 public class SI extends JFrame {
@@ -12,6 +17,73 @@ public class SI extends JFrame {
 	public SI()
 	{
 		
+		JMenuBar menubar = new JMenuBar();
+		JMenu game = new JMenu("Game");
+		
+		JMenuItem newGame = new JMenuItem("New Game");
+		JMenuItem pause = new JMenuItem("Pause");
+		JMenuItem resume = new JMenuItem("Resume");
+		JMenuItem quit = new JMenuItem("Quit");
+		
+		newGame.addActionListener(new ActionListener() 
+		{
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				panel.startNewGame();
+			}
+			
+		});
+		pause.addActionListener(new ActionListener() 
+		{
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				panel.pause();
+			}
+			
+		});
+		resume.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				panel.unpause();
+				
+			}
+			
+		});
+		quit.addActionListener(new ActionListener() 
+		{
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				displayExitDialog();
+			}
+		});
+		
+		game.add(newGame);
+		game.add(pause);
+		game.add(resume);
+		game.add(quit);
+		
+		JMenu help = new JMenu("Help");
+		
+		JMenuItem about = new JMenuItem("About");
+		
+		about.addActionListener(new ActionListener() 
+		{
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				displayAboutDialog();
+			}
+		});
+		
+		help.add(about);
+		
+		menubar.add(game);
+		menubar.add(help);
+		
+		setJMenuBar(menubar);
+		
 		setSize(500, 450);
 		setTitle("Space Invaders");
 		setResizable(false);
@@ -19,6 +91,7 @@ public class SI extends JFrame {
 		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
 		addWindowListener(new WindowAdapter() 
         {
+			// TODO Auto-generated method stub
         	@Override
         	public void windowClosing(WindowEvent arg0)
         	{
--- CPSC 280 Assignment 4/CPSC 280 Assignment 4 - Conner Davis/src/SIpanel.java
+++ CPSC 280 Assignment 4/CPSC 280 Assignment 4 - Conner Davis/src/SIpanel.java
@@ -16,5 +16,30 @@ public class SIpanel extends JPanel {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	public boolean isPaused()
+	{
+		// TODO Auto-generated method stub
+	}
+	
+	public boolean isGameInProgress()
+	{
+		// TODO Auto-generated method stub
+	}
+
+	public void startNewGame() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void unpause() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
